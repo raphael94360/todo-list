6d35a4e904a3e22df101851aa04fb5e00ba9f2af
raphael94360 <paulberthet94@gmail.com>
2022-07-17 10:48:48 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1> TODO LIST</h1>
       </header>
+      <main>
+        <div className="task">
+          {tasks.map((task, index) => {
+            return (
+              <div className="task" key={index}>
+                <input type="checkbox" /> <p>{task}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="new task"
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const tab = [...tasks];
+              tab.push(text);
+              setTasks(tab);
+              setText("");
+            }}
+          >
+            ADD TASK
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
